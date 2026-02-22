7e70419ba47ef3968f7f21e186afe251bca30cdb
Drew Barkman <djbarkman25@gmail.com>
2026-02-22 01:42:59 -0600

    we are at a breakthrough

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function requestHint() {
-    console.log(hint2.classList);
-    if (hint2.classList.contains("inactive")) {
-        hint2.classList.remove("inactive");
-    } else if (hint3.classList.contains("inactive")) {
-        hint3.classList.remove("inactive");
+    console.log(hint2box.classList);
+    if (hint2box.classList.contains("inactive")) {
+        hint2box.classList.remove("inactive");
+    } else if (hint3box.classList.contains("inactive")) {
+        hint3box.classList.remove("inactive");
         request_hint_button.classList.add("inactive");
     }
     console.log("complete")
@@ -13,10 +13,18 @@ function requestHint() {
 //     toggle win h2
 // }
 
+const arrayRange = (start, stop, step) =>
+    Array.from(
+    { length: (stop - start) / step + 1 },
+    (value, index) => start + index * step
+    );
+
+console.log(arrayRange(1, 5, 1)); // [1,2,3,4,5]
+
 const game_container = document.querySelector('#game');
-const hint1 = document.querySelector('#hint1box');
-const hint2 = document.querySelector('#hint2box');
-const hint3 = document.querySelector('#hint3box');
+const hint1box = document.querySelector('#hint1box');
+const hint2box = document.querySelector('#hint2box');
+const hint3box = document.querySelector('#hint3box');
 const request_hint_button = document.querySelector('#another-hint');
 const form = document.querySelector('#start-game-form');
 
@@ -53,3 +61,18 @@ request_hint_button.addEventListener('click', () => {
     requestHint()
 });
 
+const fakeData = {
+    'options': ['Colectivo', 'Taco Bell Cantina', 'Papa Johns', "Mooyah"],
+    'answer': 'Starbucks'
+}
+
+function loadData(data) {
+    // load options
+    secret = Math.floor(Math.random() * 5)
+    console.log(secret)
+    answer_hidden = fakeData['options']
+    // load hint one
+}
+
+const hint1 = document.querySelector('#hint1')
+// hint1.text = x
